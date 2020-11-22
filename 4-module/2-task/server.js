2bb9c858f21a6a4d9b2c9bd9e40b8f15838ca1a2
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-11-22 09:27:16 +0300

    Added teacher solution for task: Файловый сервер - запись файла

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,69 +1,38 @@
+const url = require('url');
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
-const LimitSizeStream = require('../../2-module/1-task/LimitSizeStream');
 
+const receiveFile = require('./receiveFile');
 
 const server = new http.Server();
-const origin = 'http://localhost:3000/';
 
-server.on('request', (request, response) => {
-  const pathname = new URL(request.url, origin).pathname.slice(1);
-  const limitedStream = new LimitSizeStream({limit: 1048576}); // 1Mb
+server.on('request', (req, res) => {
+  const pathname = url.parse(req.url).pathname.slice(1);
+
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (request.method) {
+  switch (req.method) {
     case 'POST':
-      if (pathname.includes('/') || pathname.includes('\\')) {
-        response.statusCode = 400;
-        response.end('400 not supported');
-      } else if (fs.existsSync(filepath) || parseInt(request.headers['content-length']) === 0) {
-        response.statusCode = 409;
-        response.end('409 file have already exist');
-      } else if (parseInt((request.headers['content-length']) > 1048576)) {
-        response.statusCode = 413;
-        response.end('413 too big file');
-      } else {
-        request.pipe(limitedStream)
-            .on('error', function(error) {
-              if (error.code === 'LIMIT_EXCEEDED') {
-                response.statusCode = 413;
-                response.end('413 too big file');
-                fs.unlink(filepath, (err) => {
-
-                });
-              } else {
-                response.statusCode = 500;
-                response.end('500 bad request');
-              }
-            });
-        const file = fs.createWriteStream(filepath);
-        limitedStream.pipe(file)
-            .on('error', function(error) {
-              response.statusCode = 500;
-              response.end('500 bad request');
-            })
-            .on('close', function() {
-              response.statusCode = 201;
-              response.end('201 file has written');
-            });
+      if (!filepath) {
+        res.statusCode = 404;
+        res.end('File not found');
+        return;
       }
+
+      receiveFile(filepath, req, res);
+
       break;
 
     default:
-      response.statusCode = 501;
-      response.end('Not implemented');
+      res.statusCode = 501;
+      res.end('Not implemented');
   }
-
-  response.on('close', function() {
-    if (response.finished === false) {
-      fs.unlink(filepath, (err) => {
-        if (err) {
-
-        }
-      });
-    }
-  });
 });
 
 module.exports = server;
